4ffd0ab1067c97d0a05d447546f57bc72345f0f9
ysimsek <simsekyasin2900@gmail.com>
2022-08-10 17:24:54 +0300

    Vue files were created

---
+++ hr-frontend/src/SignInPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row>
+    <v-col>
+      <HeadOfPage1 ></HeadOfPage1>
+      <SignIn ></SignIn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import HeadOfPage1 from '@/components/HeadOfPage1.vue'
+import SignIn from '@/components/SignIn.vue'
+
+export default {
+
+  name: 'sign-in',
+  components : {
+    SignIn,
+    HeadOfPage1,
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ hr-frontend/src/components/SignIn.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card style="display: flex; justify-content: center; margin-left: 25%; margin-right: 25%">
+    <v-col style="background-color: powderblue">
+
+      <v-card style="display: flex; justify-content: center;">
+        <v-col cols="6" style="display: flex; justify-content: center">
+          <a href="/sign-in" style="text-decoration: none;">SIGN IN</a>
+        </v-col>
+
+        <v-col cols="6" style="display: flex; justify-content: center; background-color: lightblue">
+          <a href="/" style="text-decoration: none;">LOG IN</a>
+        </v-col>
+      </v-card>
+
+
+      <v-text-field
+          label="Name - Surname"
+          hide-details="auto"
+      ></v-text-field>
+
+      <v-text-field
+          label="E-Mail"
+          :rules="rules1"
+          hide-details="auto"
+      ></v-text-field>
+
+      <v-text-field
+          label="Password">
+        :rules="rules2"
+      </v-text-field>
+
+      <v-row>
+        <v-col cols="6">
+        </v-col>
+
+        <v-col cols="6">
+          <v-btn
+              class="ma-2"
+              outlined
+              color="indigo"
+              width="100%"
+          >
+            Sign In
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    rules1: [
+      value => !!value || 'Required.',
+      value => (value && value.includes("@") && value.includes(".com")) || 'Invalid e-mail address',
+    ],
+
+    rules2: [
+      value => !!value || 'Enter at least 8 character',
+      value => (value && value.length >= 8) || 'Min 8 characters',
+    ],
+  }),
+}
+</script>
+
+<style>
+</style>
