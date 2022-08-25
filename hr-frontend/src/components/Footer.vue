500e6a476de1f8c28071be08dbc832e86e0d3e6c
ysimsek <simsekyasin2900@gmail.com>
2022-08-25 10:13:05 +0300

    I  started to design the main pages.

---
+++ hr-frontend/src/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card width="100%">
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+          flat
+          tile
+          width="100%"
+          class="flex light-blue"
+      >
+        <v-card-title height="250px">
+
+          <v-icon>
+
+          </v-icon>
+          <strong class="subheading text-center">Get connected with us on social networks!</strong>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-4"
+              icon
+          >
+            <v-icon size="24px" color="white">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-divider></v-divider>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      icons: [
+        'mdi-home',
+        'mdi-bell-outline',
+        'mdi-email',
+        'mdi-cog-outline',
+        'mdi-comment-question-outline'
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- hr-frontend/src/views/MainPage.vue
+++ hr-frontend/src/views/MainPage.vue
@@ -1,9 +1,19 @@
 <template>
-
+  <v-row>
+    <Footer></Footer>
+  </v-row>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue'
 
+export default {
+
+  name: 'main-page',
+  components : {
+    Footer
+  }
+};
 </script>
 
 <style>
